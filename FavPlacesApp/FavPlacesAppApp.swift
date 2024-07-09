7154b0aa1b6300051b8a2c0cbd9be541736e8244
brayyanchristophertrujillovalle <brayyantrujil@gmail.com>
2024-07-09 13:45:39 -0500

    Initial Commit

---
+++ FavPlacesApp/FavPlacesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavPlacesAppApp.swift
+//  FavPlacesApp
+//
+//  Created by Brayyan Christopher Trujillo Valle on 9/07/24.
+//
+
+import SwiftUI
+
+@main
+struct FavPlacesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
